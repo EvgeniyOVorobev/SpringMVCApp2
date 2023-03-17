a5b727c636526bcf41c8d56d504eaff0853b5f98
EvgeniyOVorobev <vorobiev71@yandex.ru>
2023-03-17 17:14:38 +0300

    +RequestParam

--- src/main/java/com/ev/springMVC/Controllers/MyController.java
+++ src/main/java/com/ev/springMVC/Controllers/MyController.java
@@ -3,12 +3,18 @@ package com.ev.springMVC.Controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first")
 public class MyController {
     @GetMapping("/hello")
-    public String sayHello(){
+    public String sayHello(@RequestParam(value = "name",required = false) String name,
+                           @RequestParam(value = "surname",required = false) String surname){
+
+        System.out.println("hello "+name+" "+surname);
         return "first/hello_world";
     }
 @GetMapping("/good")
